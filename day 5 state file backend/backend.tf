82da7eb009c60a48712b2d06c32eec6fc5e12e75
RayeesKhan <rayeeskhan789@gmail.com>
2025-07-10 23:29:30 -0300

    added dynamo

--- day 5 state file backend/backend.tf
+++ day 5 state file backend/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket = "rk10072025"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = false
+    #use_lockfile = false
+    dynamodb_table = "rayeestb"
     encrypt = true
   }
 }
