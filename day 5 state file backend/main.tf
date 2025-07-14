bf4c7521aa9386c2dc915d5db8482f4e9a7ffe0d
RayeesKhan <rayeeskhan789@gmail.com>
2025-07-13 21:59:54 -0300

    added day 5

--- day 5 state file backend/main.tf
+++ day 5 state file backend/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "name" {
 resource "aws_vpc" "name2" {
     cidr_block = "10.0.0.0/24"
     tags = {
-      Name = "Terraformlaptop"
+      Name = "Terraformlaptop5"
     }
   
 }
